56e835be4c90d81877096cf4884ee7560c9b6315
Dipraj Howlader <diprajhowlader2015@gmail.com>
2021-08-24 21:21:19 +0600

    new insert

---
+++ SwitchStatement.java
@@ -0,0 +1,12 @@
+package Conditional;
+
+public class SwitchStatement {
+	public static void main(String[] args) {
+		// keyword used in Switch in 
+		//switch case break default
+		
+		System.out.println("---Switch---\n");
+		
+			
+	}
+}
